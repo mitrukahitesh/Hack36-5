190e842815f7eb8bc3d8bf3485cf3ce26745f88e
Shruti Varshney <shrutivarshney1424@gmail.com>
2022-04-23 00:48:39 +0530

    customised pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage"
+import Register from "./Pages/Register"
+import Login from "./Pages/Login"
+
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
     </BrowserRouter>
   );
