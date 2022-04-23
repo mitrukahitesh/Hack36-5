e65707697d310c67ba0353220a5691138da21df5
Shruti Varshney <shrutivarshney1424@gmail.com>
2022-04-23 10:53:54 +0530

    added option page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./Pages/Homepage"
 import Register from "./Pages/Register"
 import Login from "./Pages/Login"
-
+import OptionPage from "./Pages/OptionPage";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/Login" element={<Login />} />
+          <Route path="/OptionPage" element={<OptionPage />} />
         </Routes>
     </BrowserRouter>
   );
