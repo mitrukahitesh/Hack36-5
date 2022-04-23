a0a58260169a736f7581fed9db937bb6c024a014
Shruti Mayank <shrutimayank1@gmail.com>
2022-04-24 01:42:26 +0530

    Produxt Page Updated

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import Homepage from "./Pages/Homepage"
 import Register from "./Pages/Register"
 import Login from "./Pages/Login"
 import OptionPage from "./Pages/OptionPage";
-<<<<<<< HEAD
 import TnC from "./Pages/TnC";
 import Privacy from "./Pages/Privacy";
-=======
 import AdminDashboard from "./Pages/AdminDashboard";
 import AdminProducts from "./Pages/AdminProducts";
->>>>>>> 83e992b7ce7dc95023cc1e42120f5deb8e147e52
 
 function App() {
   return (
@@ -20,13 +17,10 @@ function App() {
           <Route path="/Register" element={<Register />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/OptionPage" element={<OptionPage />} />
-<<<<<<< HEAD
           <Route path="/TnC" element={<TnC />} />
           <Route path="/Privacy" element={<Privacy />} />
-=======
           <Route path="/Admin/Dashboard" element={<AdminDashboard />} />
           <Route path="/Admin/Products" element={<AdminProducts />} />
->>>>>>> 83e992b7ce7dc95023cc1e42120f5deb8e147e52
         </Routes>
       </div>
     </BrowserRouter>
